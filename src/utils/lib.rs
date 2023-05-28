3712dd9a426d6efb2c754c29eef8b2eca0c6bcf2
Seiya Nuta <nuta@seiya.me>
2023-05-29 00:02:28 +0900

    Update

--- src/utils/lib.rs
+++ src/utils/lib.rs
@@ -1,4 +1,7 @@
 //! A self-contained utilities for both the kernel and user programs.
 #![no_std]
 
+#[macro_use]
+pub mod macros;
+
 pub mod alignment;
---
+++ src/utils/macros.rs
@@ -0,0 +1,22 @@
+/// Compile-time assertion.
+///
+/// # Examples
+///
+/// ```
+/// use utils::static_assert;
+///
+/// static_assert!(1 + 1 == 2);
+/// static_assert!(1 + 1 == 2, "1 + 1 should be 2");
+/// ```
+///
+/// https://github.com/rust-lang/rfcs/issues/2790
+/// https://github.com/rust-lang/rust/pull/89508
+#[macro_export]
+macro_rules! static_assert {
+    ($cond:expr, $($args:tt)+) => {
+        const _: () = ::core::assert!($cond, $($args)+);
+    };
+    ($cond:expr) => {
+        const _: () = ::core::assert!($cond);
+    };
+}
