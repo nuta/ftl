89ae96b48734d9ae8ac2b8dad96e8a2a18e1ff56
Seiya Nuta <nuta@seiya.me>
2023-07-08 15:24:34 +0900

    Update

--- src/kernel/process.rs
+++ src/kernel/process.rs
@@ -2,17 +2,33 @@ use core::mem::size_of;
 
 use utils::static_assert;
 
-use crate::{thread::Thread, arch::{self, PAGE_SIZE}, ref_count::Ref, object::KernelObject};
+use crate::{ arch::{self, PAGE_SIZE}, ref_count::Ref, object::KernelObject};
 
-pub struct Handle {
-    object: Ref<dyn KernelObject>,
+/// Allowed operations on a handle.
+///
+/// TODO: Use bitfields to save space.
+pub struct Rights {
+    pub read: bool,
+    pub write: bool,
 }
 
+/// A reference to a kernel object with associated rights, aka *capability*.
+pub struct Handle {
+    object: Ref<dyn KernelObject>,
+    rights: Rights,
+}
+
+/// The process control block (PCB).
+///
+/// A process is a collection of threads and resources (page tables and handles)
+/// that are shared among the threads.
 pub struct Process {
     page_table: arch::PageTable,
     handles: [Handle; 256],
-    //  TODO:
-    // indirect_handles: [HandleTable; 16],
 }
 
+// We want to keep the size of `Process` small so that a process can be
+// created as cheaply as possible. If we come to a point where we need
+// more handles, let's consider adding a second-level handle table,
+// similar to indirect blocks in a file system.
 static_assert!(size_of::<Process>() <= PAGE_SIZE);
