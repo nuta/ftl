6b60a09e9a075e5a4667cae5e1399e7a7a8049ed
Seiya Nuta <nuta@seiya.me>
2023-07-09 12:35:15 +0900

    Update

--- kernel/ref_count.rs
+++ kernel/ref_count.rs
@@ -1,4 +1,4 @@
-use core::{ops::Deref, ptr::NonNull};
+use core::{ops::Deref, ptr::NonNull, sync::atomic::AtomicUsize};
 
 pub unsafe trait RefCounted {
     fn inc_ref(&self);
@@ -9,10 +9,27 @@ pub struct Ref<T: RefCounted + ?Sized> {
     ptr: NonNull<T>,
 }
 
+impl<T: RefCounted + ?Sized> Ref<T> {
+    pub const fn new(ptr: NonNull<T>) -> Ref<T> {
+        Ref {
+            ptr,
+        }
+    }
+
+    pub fn inc_ref(&self) -> Self {
+        // Safety: `self.ptr` is valid and the reference counter guarantees
+        //         that the referenced value is still alive.
+        unsafe { self.ptr.as_ref() }.inc_ref();
+        Self { ptr: self.ptr }
+    }
+}
+
 impl<T: RefCounted + ?Sized> Deref for Ref<T> {
     type Target = T;
 
     fn deref(&self) -> &T {
+        // Safety: `self.ptr` is valid and the reference counter guarantees
+        //         that the referenced value is still alive.
         unsafe { self.ptr.as_ref() }
     }
 }
