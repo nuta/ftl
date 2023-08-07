60aab966d2ad67c614f7ad1e26e9190afaf1a69d
Seiya Nuta <nuta@seiya.me>
2023-08-08 02:44:39 +0900

    Update

--- kernel/main.rs
+++ kernel/main.rs
@@ -66,41 +66,6 @@ pub fn kernel_main() {
         unreachable!();
     }
 
-    let mut v = alloc::vec::Vec::new();
-    v.push(1);
-    v.push(2);
-    v.push(3);
-    println!("{:#?}", v);
-
-    println!("\n\n\x1b[1;35mHello from Rust World!\x1b[0m\n\n");
-
-    use core::mem::size_of;
-    println!("size_of::<Process>(): {}", size_of::<process::Process>());
-    println!("size_of::<Handle>(): {}", size_of::<process::Handle>());
-    println!("size_of::<Thread>(): {}", size_of::<thread::Thread>());
-    println!(
-        "size_of::<SharedRef<u8>>(): {}",
-        size_of::<ref_count::SharedRef<u8>>()
-    );
-    println!(
-        "size_of::<SharedRefHeader>(): {} (align={})",
-        size_of::<ref_count::SharedObject::<u8>>(),
-        core::mem::align_of::<ref_count::SharedObject::<u8>>()
-    );
-
-    // unsafe extern "C" fn first_user_program() {
-    //     core::arch::asm!("nop; li a0, 0xc0be; ecall; li a0, 0xbeef; ecall; li a0, 0xdead; ecall");
-    // }
-
-    // *(InterruptCounter.borrow_mut()) = 456;
-    // println!("intr: {}", *InterruptCounter.borrow());
-
-    // use alloc::boxed::Box;
-    // let t =
-    //     Box::new(arch::Thread::new(first_user_program as *const () as usize));
-    // arch::Thread::set_current_thread(t);
-    // arch::Thread::switch_test();
-
     let mut pagetable =
         memory::allocate_and_initialize(PAGE_SIZE, |pool, vaddr| {
             UniqueRef::new(
