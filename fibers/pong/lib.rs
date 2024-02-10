e841e7702530a314c51856e233050fcab1720c65
Seiya Nuta <nuta@seiya.me>
2024-02-10 23:54:39 +0900

    Update

--- fibers/pong/lib.rs
+++ fibers/pong/lib.rs
@@ -1,3 +1,11 @@
 #![no_std]
 
-pub fn main() {}
+use ftl_api::println;
+
+pub fn main() {
+    println!("fiber B: world");
+    for i in 0.. {
+        ftl_api::thread::yield_cpu();
+        println!("fiber B: {}", i);
+    }
+}
