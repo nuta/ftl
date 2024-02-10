cc8196be064c7ea00a3ba21f36384bd46b5b512c
Seiya Nuta <nuta@seiya.me>
2024-02-10 18:50:18 +0900

    Update

--- fibers/ping/lib.rs
+++ fibers/ping/lib.rs
@@ -2,15 +2,10 @@
 
 use ftl_api::println;
 
-#[link_section = ".ftl.fiber_mains"]
-pub static __MAIN: extern "C" fn() = {
-    extern "C" fn main() {
-        println!("fiber A: hello");
-        for i in 0.. {
-            ftl_api::thread::yield_cpu();
-            println!("fiber A: {}", i);
-        }
+pub fn main() {
+    println!("fiber A: hello");
+    for i in 0.. {
+        ftl_api::thread::yield_cpu();
+        println!("fiber A: {}", i);
     }
-
-    main
-};
+}
