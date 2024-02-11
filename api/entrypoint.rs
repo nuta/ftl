0cfd7ed90538b643e96bcd44463594fb3a327ee0
Seiya Nuta <nuta@seiya.me>
2024-02-11 22:07:28 +0900

    Update

--- api/entrypoint.rs
+++ api/entrypoint.rs
@@ -3,7 +3,7 @@ use crate::environ::Environ;
 pub extern "C" fn fiber_entrypoint(environ_cstr: *const i8, main: fn(Environ)) {
     let environ_cstr = unsafe { core::ffi::CStr::from_ptr(environ_cstr) };
     let environ_str = environ_cstr.to_str().expect("environ is not valid string");
-    let raw_environ = todo!();
+    let raw_environ = serde_json::from_str(environ_str).expect("environ is not valid JSON");
     let environ = Environ::from_raw(raw_environ);
     main(environ);
 }
