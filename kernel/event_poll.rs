67fce4c45694aa37e0838dbfa5b0a0b83923e4a3
Seiya Nuta <nuta@seiya.me>
2024-02-17 19:20:26 +0900

    Update

--- kernel/event_poll.rs
+++ kernel/event_poll.rs
@@ -4,3 +4,24 @@ use hashbrown::HashMap;
 pub struct EventPoll {
     pending: HashMap<HandleId, Event>,
 }
+
+impl EventPoll {
+    pub fn new() -> Self {
+        Self {
+            pending: HashMap::new(),
+        }
+    }
+
+    pub fn add_event(&mut self, handle_id: HandleId, event: Event) {
+        let e = self
+            .pending
+            .entry(handle_id)
+            .or_insert_with(|| Event::zeroed());
+
+        *e |= event;
+    }
+
+    pub fn poll(&mut self) -> Option<(HandleId, Event)> {
+        self.pending.iter().next().map(|(k, v)| (*k, v.clone()))
+    }
+}
