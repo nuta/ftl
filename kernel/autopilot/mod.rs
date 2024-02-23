0d600fafc4d92d98820407436fa8c9422cf8c47c
Seiya Nuta <nuta@seiya.me>
2024-02-24 02:23:59 +0900

    Update

--- kernel/autopilot/mod.rs
+++ kernel/autopilot/mod.rs
@@ -1,11 +1,13 @@
 use alloc::collections::BTreeMap;
 use alloc::ffi::CString;
+use alloc::string::String;
 use alloc::string::ToString;
 use alloc::sync::Arc;
 use alloc::vec::Vec;
 
 use ftl_types::environ::Environ;
 use ftl_types::handle::HandleId;
+use ftl_types::message::Message;
 use ftl_types::spec::DeviceTreeEntry;
 use ftl_types::spec::FiberSpec;
 
@@ -17,20 +19,31 @@ use crate::lock::Mutex;
 
 mod device_tree;
 
+struct ResolvedDep {
+    provide_name: String,
+    client_channel: Channel,
+}
+
 struct DesiredFiber {
     spec: FiberSpec,
     main: fn(*const i8),
-    their_control_ch: Channel,
+    resolved_deps: Vec<ResolvedDep>,
     our_control_ch: Channel,
 }
 
 struct Desired {
     fibers: Vec<DesiredFiber>,
+    server_channels: BTreeMap<String /* provide name */, Vec<Channel>>,
+    client_channels: BTreeMap<String /* provide name */, Vec<Channel>>,
 }
 
 impl Desired {
     pub fn new() -> Desired {
-        Desired { fibers: Vec::new() }
+        Desired {
+            fibers: Vec::new(),
+            server_channels: BTreeMap::new(),
+            client_channels: BTreeMap::new(),
+        }
     }
 
     pub fn load_bootinfo(&mut self, bootinfo: &BootInfo) {
@@ -38,15 +51,56 @@ impl Desired {
     }
 
     fn visit_kernel_fibers(&mut self, bootinfo: &BootInfo) {
+        let mut fiber_specs = BTreeMap::new();
+        let mut kernel_fiber_entries = BTreeMap::new();
         for (spec, main) in bootinfo.kernel_fibers.iter() {
             let spec: FiberSpec =
                 serde_json::from_str(spec).expect("failed to parse an in-kernel fiber spec");
+
+            let fiber_name = spec.name.clone();
+            fiber_specs.insert(fiber_name.clone(), spec);
+            kernel_fiber_entries.insert(fiber_name, main);
+        }
+
+        for (fiber_name, spec) in fiber_specs.iter() {
             let (control_ch1, control_ch2) = Channel::new().unwrap();
+            let mut resolved_deps = Vec::new();
+            resolved_deps.push(ResolvedDep {
+                provide_name: "autopilot".to_string(),
+                client_channel: control_ch1,
+            });
+
+            for dep in &spec.deps {
+                if dep == "autopilot" {
+                    continue;
+                }
+
+                let Some(dep_fiber) = fiber_specs
+                    .values()
+                    .find(|spec| spec.provides.iter().any(|provide| provide == dep))
+                else {
+                    panic!(
+                        "fiber {} depends on {}, but no such fiber exists",
+                        fiber_name, dep
+                    );
+                };
+
+                let (client_ch, server_ch) = Channel::new().unwrap();
+                self.server_channels
+                    .entry(dep_fiber.name.clone())
+                    .or_insert(Vec::new())
+                    .push(server_ch);
+
+                resolved_deps.push(ResolvedDep {
+                    provide_name: dep.to_string(),
+                    client_channel: client_ch,
+                });
+            }
 
             self.fibers.push(DesiredFiber {
-                spec,
-                main: *main,
-                their_control_ch: control_ch1,
+                spec: fiber_specs[fiber_name].clone(),
+                main: *kernel_fiber_entries[fiber_name],
+                resolved_deps,
                 our_control_ch: control_ch2,
             });
         }
@@ -90,7 +144,7 @@ impl Actual {
         None
     }
 
-    pub fn apply_desired(&mut self, desired: Desired) {
+    pub fn apply_desired(&mut self, mut desired: Desired) {
         debug_assert!(
             self.fibers.is_empty(),
             "TODO: diffing states are not implemented"
@@ -99,8 +153,8 @@ impl Actual {
         for DesiredFiber {
             spec,
             main,
-            their_control_ch,
             our_control_ch,
+            resolved_deps,
         } in desired.fibers
         {
             let mut device = None;
@@ -140,18 +194,27 @@ impl Actual {
 
             let mut fiber = Fiber::new();
             let mut deps = BTreeMap::new();
-            let mut autopilot_ch = Some(their_control_ch);
-            for dep_name in spec.deps.iter() {
+            for ResolvedDep {
+                provide_name,
+                client_channel,
+            } in resolved_deps
+            {
                 let handle_id = alloc_handle_id();
-                let object = match dep_name.as_str() {
-                    "autopilot" => Object::Channel(autopilot_ch.take().unwrap()),
-                    _ => {
-                        todo!();
-                    }
-                };
+                fiber.insert_handle(handle_id, Object::Channel(client_channel));
+                deps.insert(provide_name.to_string(), handle_id);
+            }
 
-                fiber.insert_handle(handle_id, object);
-                deps.insert(dep_name.to_string(), handle_id);
+            if let Some(server_channels) = desired.server_channels.remove(&spec.name) {
+                for ch in server_channels {
+                    let handle_id = alloc_handle_id();
+                    fiber.insert_handle(handle_id, Object::Channel(ch));
+
+                    // Non-blocking send to the fiber to tell it about the
+                    // pre-existing client channels.
+                    our_control_ch
+                        .send(Message::NewClient { ch: handle_id })
+                        .unwrap();
+                }
             }
 
             let environ = Environ { deps, device };
