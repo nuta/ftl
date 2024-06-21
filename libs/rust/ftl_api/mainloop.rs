a6042c4d1526d587bc5f852c20f9bce4094fbfce
Seiya Nuta <nuta@seiya.me>
2024-06-21 14:31:57 +0900

    Update

--- libs/rust/ftl_api/mainloop.rs
+++ libs/rust/ftl_api/mainloop.rs
@@ -1,13 +1,18 @@
+use ftl_types::handle::HandleId;
+use hashbrown::HashMap;
+
 use crate::poll::Poll;
 
 pub struct Mainloop<State> {
     poll: Poll,
+    states: HashMap<HandleId, State>,
 }
 
 impl<State> Mainloop<State> {
     pub fn new() -> Mainloop<State> {
         Mainloop {
             poll: Poll::new().unwrap(),
+            states: HashMap::new(),
         }
     }
 }
