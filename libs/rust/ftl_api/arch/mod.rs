c552261961982862ab720919a7da5aaa9ca8f207
Seiya Nuta <nuta@seiya.me>
2024-06-24 09:50:48 +0900

    Update

--- libs/rust/ftl_api/arch/mod.rs
+++ libs/rust/ftl_api/arch/mod.rs
@@ -1,5 +1,11 @@
 #[cfg(all(target_arch = "riscv64"))]
 mod riscv64;
 
+#[cfg(all(target_arch = "x86_64"))]
+mod x64;
+
 #[cfg(all(target_arch = "riscv64"))]
 pub use riscv64::*;
+
+#[cfg(all(target_arch = "x86_64"))]
+pub use x64::*;
