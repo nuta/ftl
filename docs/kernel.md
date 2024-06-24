be7ba192e2c257faaf803eadcb7f6b79f752e886
Seiya Nuta <nuta@seiya.me>
2024-06-24 12:42:57 +0900

    Update

--- docs/kernel.md
+++ docs/kernel.md
@@ -9,3 +9,46 @@
 Sends a message. The message will be delivered to `ch`'s peer.
 
 If the kernel fails returns an error code, it's guaranteed that `handles` will not be transferred. That is, **the caller must keep ownership of the handles**. This is why `Channel::send` returns `SendError` instead of `FtlError`.
+
+# Layout
+
+```plain
+
+|63 or 31                                      14|13   12|11         0|
++------------------------------------------------+-------+------------+
+|                       TYPE                     |   H   |    LEN     |
++------------------------------------------------+-------+------------+
+
+LEN  (12 bits) - # of bytes in the message data.
+H    (2 bits)  - # of handles in the message.
+TYPE (rest)    - Message type.
+
+```
+
+# Design decisions
+
+## A single `isize` for `TYPE`, `H`, and `LEN`
+
+This is because:
+
+- It can fit in a single CPU register. Specifying the message metadata
+  can be done with simply setting an immediate value to a register.
+
+- The message structure (i.e. its length and the number of handles) can be
+  validated at once when checking the message type.
+
+- It forcibly limits the message data length and the number of handles, in
+  other words, the kernel just need to do bitwise AND operations to get them,
+  without any additional checks.
+
+## Message metadata is not part of the message buffer
+
+This is because:
+
+- If it was part of the message buffer, the kernel would need to read the
+  message buffer first to determine the number/length of handles/data that
+  it needs to copy.
+
+- It's useful for debugging. We can determine the message type even if an
+  app accidentally passed an invalid pointer to the kernel. It could be a
+  key clue for debugging.
--- libs/rust/ftl_types/message.rs
+++ libs/rust/ftl_types/message.rs
@@ -2,6 +2,50 @@ use core::mem::size_of;
 
 use crate::handle::HandleId;
 
+/// The message metadata.
+///
+/// # Layout
+///
+/// ```plain
+///
+/// |63 or 31                                      14|13   12|11         0|
+/// +------------------------------------------------+-------+------------+
+/// |                       TYPE                     |   H   |    LEN     |
+/// +------------------------------------------------+-------+------------+
+///
+/// LEN  (12 bits) - # of bytes in the message data.
+/// H    (2 bits)  - # of handles in the message.
+/// TYPE (rest)    - Message type.
+///
+/// ```
+///
+/// # Design decisions
+///
+/// ## A single `isize` for `TYPE`, `H`, and `LEN`
+///
+/// This is because:
+///
+/// - It can fit in a single CPU register. Specifying the message metadata
+///   can be done with simply setting an immediate value to a register.
+///
+/// - The message structure (i.e. its length and the number of handles) can be
+///   validated at once when checking the message type.
+///
+/// - It forcibly limits the message data length and the number of handles, in
+///   other words, the kernel just need to do bitwise AND operations to get them,
+///   without any additional checks.
+///
+/// ## Message metadata is not part of the message buffer
+///
+/// This is because:
+///
+/// - If it was part of the message buffer, the kernel would need to read the
+///   message buffer first to determine the number/length of handles/data that
+///   it needs to copy.
+///
+/// - It's useful for debugging. We can determine the message type even if an
+///   app accidentally passed an invalid pointer to the kernel. It could be a
+///   key clue for debugging.
 #[derive(Clone, Copy, PartialEq, Eq, Debug)]
 #[repr(transparent)]
 pub struct MessageInfo(isize);
@@ -13,6 +57,14 @@ impl MessageInfo {
 
     pub const fn as_raw(self) -> isize {
         self.0
+    }
+
+    pub const fn message_type(self) -> isize {
+        self.0 >> 14
+    }
+
+    pub const fn num_handles(self) -> usize {
+        self.0 as usize >> 12 & 0b11
     }
 
     pub const fn data_len(self) -> usize {
