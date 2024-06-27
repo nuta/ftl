059e35a4e19b9f0400dd20c283a283485049a51f
Seiya Nuta <nuta@seiya.me>
2024-06-27 19:58:57 +0900

    Update

--- kernel/boot.rs
+++ kernel/boot.rs
@@ -1,8 +1,10 @@
-use alloc::borrow::ToOwned;
+use alloc::string::String;
 use alloc::vec;
 
 use arrayvec::ArrayVec;
 use ftl_types::spec::AppSpec;
+use ftl_types::spec::Spec;
+use ftl_types::spec::SpecFile;
 use ftl_utils::byte_size::ByteSize;
 
 use crate::arch;
@@ -38,21 +40,21 @@ pub fn boot(cpu_id: CpuId, bootinfo: BootInfo) -> ! {
     process::init();
     cpuvar::percpu_init(cpu_id);
 
-    fn load_app_spec(spec: &[u8]) -> AppSpec {
-        serde_json::from_slice(spec).expect("failed to parse app spec")
+    fn load_app_spec(spec: &[u8], elf_file: &'static [u8]) -> (String, AppSpec, &'static [u8]) {
+        let spec_file: SpecFile = serde_json::from_slice(spec).expect("failed to parse app spec");
+        let Spec::App(app_spec) = spec_file.spec;
+        (spec_file.name, app_spec, elf_file)
     }
 
     let mut autopilot = Autopilot::new();
     autopilot
         .start_apps(vec![
-            (
-                "ping".to_owned(),
-                load_app_spec(include_bytes!("../apps/ping/app.spec.json")),
+            load_app_spec(
+                include_bytes!("../apps/ping/app.spec.json"),
                 include_bytes!("../build/apps/ping.elf"),
             ),
-            (
-                "pong".to_owned(),
-                load_app_spec(include_bytes!("../apps/pong/app.spec.json")),
+            load_app_spec(
+                include_bytes!("../apps/pong/app.spec.json"),
                 include_bytes!("../build/apps/pong.elf"),
             ),
         ])
