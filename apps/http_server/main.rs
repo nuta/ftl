7732075c0cd8be1ab98ff057849a2e6a704f6b01
Seiya Nuta <nuta@seiya.me>
2024-07-28 12:02:58 +0900

    Update

--- apps/http_server/main.rs
+++ apps/http_server/main.rs
@@ -12,57 +12,39 @@ use ftl_api_autogen::apps::http_server::Environ;
 use ftl_api_autogen::apps::http_server::Message;
 
 #[derive(Debug)]
-enum State {
-    Headers,
-    Body,
-    Eof,
-}
-
-#[derive(Debug)]
 struct Client {
-    state: State,
     buffered: Vec<u8>,
 }
 
 impl Client {
     pub fn new() -> Client {
         Client {
-            state: State::Headers,
             buffered: Vec::new(),
         }
     }
 
     pub fn receive(&mut self, data: &[u8]) {
-        match self.state {
-            State::Headers => {
-                self.buffered.extend_from_slice(&data);
-                if let Some(index) = self.buffered.windows(4).position(|w| w == b"\r\n\r\n") {
-                    let request_bytes = &self.buffered[..index + 4];
+        self.buffered.extend_from_slice(&data);
+        if let Some(index) = self.buffered.windows(4).position(|w| w == b"\r\n\r\n") {
+            let request_bytes = &self.buffered[..index + 4];
 
-                    let mut headers = [httparse::EMPTY_HEADER; 32];
-                    let mut request = httparse::Request::new(&mut headers);
-                    match request.parse(request_bytes) {
-                        Ok(httparse::Status::Complete(len)) => {
-                            trace!("parsed request: {:?}", request);
-                            self.state = State::Body;
-                            self.buffered = Vec::new();
-                        }
-                        Ok(httparse::Status::Partial) => {
-                            warn!("partial request");
-                            return;
-                        }
-                        Err(e) => {
-                            warn!("error parsing request: {:?}", e);
-                            return;
-                        }
-                    };
+            let mut headers = [httparse::EMPTY_HEADER; 32];
+            let mut request = httparse::Request::new(&mut headers);
+            match request.parse(request_bytes) {
+                Ok(httparse::Status::Complete(len)) => {
+                    trace!("parsed request: {:?}", request);
+                    self.state = State::Body;
+                    self.buffered = Vec::new();
                 }
-            }
-            State::Body => {
-            }
-            State::Eof => {
-                warn!("received data after EOF");
-            }
+                Ok(httparse::Status::Partial) => {
+                    warn!("partial request");
+                    return;
+                }
+                Err(e) => {
+                    warn!("error parsing request: {:?}", e);
+                    return;
+                }
+            };
         }
     }
 }
@@ -92,7 +74,9 @@ pub fn main(mut env: Environ) {
                 match (ctx, m) {
                     (Context::Tcpip, Message::TcpAccepted(m)) => {
                         let ch = Channel::from_handle(OwnedHandle::from_raw(m.sock()));
-                        mainloop.add_channel(ch, Context::TcpSock(Client::new())).unwrap();
+                        mainloop
+                            .add_channel(ch, Context::TcpSock(Client::new()))
+                            .unwrap();
                     }
                     (Context::TcpSock(client), Message::TcpReceived(m)) => {
                         client.receive(m.data().as_slice());
