b1f851c363c1ada1de01b737265750a8ba969a7a
Seiya Nuta <nuta@seiya.me>
2024-09-06 19:24:53 +0900

    Update

--- apps/ping_client/main.rs
+++ apps/ping_client/main.rs
@@ -6,17 +6,17 @@ ftl_api::autogen!();
 use ftl_api::environ::Environ;
 use ftl_api::prelude::*;
 use ftl_api::types::message::MessageBuffer;
-use ftl_api_autogen::protocols::ping::PingReply;
-use ftl_api_autogen::protocols::ping::PingRequest;
+use ftl_autogen::ping::PingReply;
+use ftl_autogen::ping::Ping;
 
 #[no_mangle]
 pub fn main(mut env: Environ) {
     let ch = env.take_channel("dep:ping_server").unwrap();
     let mut msgbuffer = MessageBuffer::new();
     loop {
-        ch.send(PingRequest { value: 42 }).unwrap();
+        ch.send(Ping { value: 42 }).unwrap();
 
         let reply = ch.recv_with_buffer::<PingReply>(&mut msgbuffer).unwrap();
-        info!("received {}", reply.value());
+        info!("received {}", reply.value);
     }
 }
