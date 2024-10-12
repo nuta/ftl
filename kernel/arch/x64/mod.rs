fee96bea71c58c9ea630749fb015c4dc19e61c1c
Seiya Nuta <nuta@seiya.me>
2024-10-12 13:30:45 +0900

    Update

--- kernel/arch/x64/mod.rs
+++ kernel/arch/x64/mod.rs
@@ -30,6 +30,7 @@ pub use cpuvar::CpuVar;
 pub use idle::idle;
 pub use init::early_init;
 pub use init::init;
+pub use switch::kernel_syscall_entry;
 pub use switch::return_to_user;
 pub use thread::Thread;
 pub use vmspace::VmSpace;
@@ -62,72 +63,6 @@ where
     F: FnMut(usize),
 {
     println!("backtrace not implemented")
-}
-
-pub unsafe extern "C" fn kernel_syscall_entry(
-    _a0: isize,
-    _a1: isize,
-    _a2: isize,
-    _a3: isize,
-    _a4: isize,
-    _a5: isize,
-) -> isize {
-    asm!(
-        r#"
-            mov rax, gs:[{context_offset}]
-
-            // Save general-purpose registers.
-            mov [rax + {rbx_offset}], rbx
-            mov [rax + {rcx_offset}], rcx
-            mov [rax + {rdx_offset}], rdx
-            mov [rax + {rsi_offset}], rsi
-            mov [rax + {rdi_offset}], rdi
-            mov [rax + {rbp_offset}], rbp
-            mov [rax + {rsp_offset}], rsp
-            mov [rax + {r8_offset}],  r8
-            mov [rax + {r9_offset}],  r9
-            mov [rax + {r10_offset}], r10
-            mov [rax + {r11_offset}], r11
-            mov [rax + {r12_offset}], r12
-            mov [rax + {r13_offset}], r13
-            mov [rax + {r14_offset}], r14
-            mov [rax + {r15_offset}], r15
-
-            // Get the return address.
-            mov rax, [rsp]
-            mov gs:[{rip_offset}], rax
-
-            // Handle the system call.
-            call {syscall_handler}
-
-            // Save the return value in the thread context, and switch
-            // to the next thread.
-            mov rdi, gs:[{context_offset}]
-            mov [rdi + {rax_offset}], rax
-            jmp {switch_to_next}
-        "#,
-        context_offset = const offset_of!(crate::arch::CpuVar, context),
-        rip_offset = const offset_of!(Context, rip),
-        rax_offset = const offset_of!(Context, rax),
-        rbx_offset = const offset_of!(Context, rbx),
-        rcx_offset = const offset_of!(Context, rcx),
-        rdx_offset = const offset_of!(Context, rdx),
-        rsi_offset = const offset_of!(Context, rsi),
-        rdi_offset = const offset_of!(Context, rdi),
-        rbp_offset = const offset_of!(Context, rbp),
-        rsp_offset = const offset_of!(Context, rsp),
-        r8_offset = const offset_of!(Context, r8),
-        r9_offset = const offset_of!(Context, r9),
-        r10_offset = const offset_of!(Context, r10),
-        r11_offset = const offset_of!(Context, r11),
-        r12_offset = const offset_of!(Context, r12),
-        r13_offset = const offset_of!(Context, r13),
-        r14_offset = const offset_of!(Context, r14),
-        r15_offset = const offset_of!(Context, r15),
-        syscall_handler = sym crate::syscall::syscall_handler,
-        switch_to_next = sym crate::thread::switch_thread,
-        options(noreturn)
-    )
 }
 
 pub fn interrupt_create(interrupt: &SharedRef<Interrupt>) -> Result<(), FtlError> {
