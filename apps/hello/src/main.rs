521a3071147bfc6235b3e7247c622de13e979b06
Seiya Nuta <nuta@seiya.me>
2026-01-27 15:08:37 +0900

    Update

--- apps/hello/src/main.rs
+++ apps/hello/src/main.rs
@@ -4,7 +4,9 @@ use ftl::println;
 
 #[unsafe(no_mangle)]
 fn main() {
-    println!("Hello world from system call!");
+    println!(
+        "\x1b[1m\x1b[32mHello\x1b[0m\x1b[1m \x1b[1m\x1b[33mworld\x1b[0m\x1b[1m \x1b[1m\x1b[36mfrom\x1b[0m\x1b[1m \x1b[1m\x1b[35msystem call!\x1b[0m\x1b[1m\x1b[0m"
+    );
     loop {
         unsafe { core::arch::asm!("hlt") }
     }
