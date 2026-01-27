74ed54ddf2924e0f8a8c74dacba6957f44e28de4
Seiya Nuta <nuta@seiya.me>
2026-01-27 22:34:17 +0900

    Update

--- apps/hello/src/main.rs
+++ apps/hello/src/main.rs
@@ -15,11 +15,18 @@ fn main() {
     ftl::pci::sys_pci_lookup(entries.as_mut_ptr() as *mut PciEntry, 10, 0x1af4, 0x1000).unwrap();
 
     let entryies = unsafe { entries.assume_init() };
-    for i in 0..1 {
-        println!("{:x}:{:x}", entryies[i].bus, entryies[i].slot);
+    println!("got {} PCI entries", entryies.len());
+    for entry in entryies {
+        println!("{:x}:{:x}", entry.bus, entry.slot);
     }
+
+    println!("setting busmaster for {:x}:{:x}", entryies[0].bus, entryies[0].slot);
+    ftl::pci::sys_pci_set_busmaster(entryies[0].bus, entryies[0].slot, 1).unwrap();
+
+    println!("getting BAR for {:x}:{:x}", entryies[0].bus, entryies[0].slot);
+    let bar = ftl::pci::sys_pci_get_bar(entryies[0].bus, entryies[0].slot, 0).unwrap();
+    println!("BAR: {:x}", bar);
 
     loop {
         unsafe { core::arch::asm!("hlt") }
-    }
 }
