35e7c826803c1bbcd9658404317b0035f6f9336f
Seiya Nuta <nuta@seiya.me>
2026-01-27 12:38:38 +0900

    Update

--- kernel/src/loader.rs
+++ kernel/src/loader.rs
@@ -20,6 +20,18 @@ struct Ehdr64 {
     shstrndx: u16,
 }
 
+#[repr(C)]
+struct Phdr64 {
+    type_: u32,
+    flags: u32,
+    offset: u64,
+    vaddr: u64,
+    paddr: u64,
+    filesz: u64,
+    memsz: u64,
+    align: u64,
+}
+
 fn load_file(file: initfs::File) {
     let ehdr = unsafe { &*(file.data.as_ptr() as *const Ehdr64) };
     if ehdr.magic[..4] != [0x7f, b'E', b'L', b'F'] {
@@ -29,6 +41,20 @@ fn load_file(file: initfs::File) {
     }
 
     println!("{}: ELF entry={:x}", file.name, ehdr.entry);
+    let phdrs = unsafe {
+        let ptr = (file.data.as_ptr().add(ehdr.phoff as usize) as *const Phdr64);
+        core::slice::from_raw_parts(ptr, ehdr.phnum as usize)
+    };
+
+    for phdr in phdrs {
+        println!("{}: PHDR type={:x}, flags={:x}, offset={:x}, vaddr={:x}, paddr={:x}, filesz={:x}, memsz={:x}, align={:x}",
+            file.name,
+            phdr.type_,
+            phdr.flags,
+            phdr.offset,
+            phdr.vaddr,
+            phdr.paddr,
+        );
 }
 
 pub fn load(initfs: &InitFs) {
