1a73152ddd4d2d9a32e75a1a35f3c9f2d4a7058a
Seiya Nuta <nuta@seiya.me>
2026-02-07 16:18:09 +0900

    Fix `deploy-to-google-cloud.sh`

--- tools/deploy-to-google-cloud.sh
+++ tools/deploy-to-google-cloud.sh
@@ -15,7 +15,7 @@ BUCKET_PATH="gs://$1/ftl.tar.gz"
 # brew install i686-elf-grub
 GRUB_MKRESCUE=/opt/homebrew/opt/i686-elf-grub/bin/i686-elf-grub-mkrescue
 
-BUILD_ONLY=1 ./run.sh
+bin/ftl build
 
 mkdir -p isofiles/boot/grub
 cp kernel/src/arch/x64/grub.cfg isofiles/boot/grub/
