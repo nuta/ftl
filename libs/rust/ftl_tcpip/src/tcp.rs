9877375b1f44dc9da7a11fae03ca5943a9a92404
Seiya Nuta <nuta@seiya.me>
2026-04-21 23:14:43 +0900

    Update

--- libs/rust/ftl_tcpip/src/tcp.rs
+++ libs/rust/ftl_tcpip/src/tcp.rs
@@ -3,26 +3,20 @@ use alloc::collections::VecDeque;
 use crate::{Io, OutOfMemoryError};
 use crate::socket::AnySocket;
 
-pub trait Read {
-    type Error;
+pub enum Error {}
 
-    fn write(self, buf: &[u8]) -> Result<(), Self::Error>;
-    fn error(self, error: Self::Error) -> Result<(), Self::Error>;
-    fn complete(self, read_len: usize) -> Result<(), Self::Error>;
+pub trait Read: Send + Sync {
+    fn write(&mut self, buf: &[u8]) -> usize;
+    fn complete(self, result: Result<usize, Error>);
 }
 
-pub trait Write {
-    type Error;
-
-    fn read(&mut self, buf: &mut [u8]) -> Result<usize, Self::Error>;
-    fn error(self, error: Self::Error) -> Result<(), Self::Error>;
-    fn complete(self, written_len: usize) -> Result<(), Self::Error>;
+pub trait Write: Send + Sync {
+    fn read(&mut self, buf: &mut [u8]) -> usize;
+    fn complete(self, result: Result<usize, Error>);
 }
 
-pub trait Accept: Send + Sync + Sized + 'static {
-    type Error;
-
-    fn complete(self) -> Result<(), Self::Error>;
+pub trait Accept: Send + Sync {
+    fn complete(self, result: Result<(), Error>);
 }
 
 pub struct TcpConn<I: Io> {
@@ -38,6 +32,8 @@ impl<I: Io> TcpConn<I> {
         }
     }
 }
+
+impl<I: Io> AnySocket for TcpConn<I> {}
 
 pub struct TcpListener<I: Io> {
     pending_accepts: VecDeque<I::TcpAccept>,
