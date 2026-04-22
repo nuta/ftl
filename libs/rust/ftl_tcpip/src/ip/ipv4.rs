50b780db3131b6a16a305cf81587188d398801af
Seiya Nuta <nuta@seiya.me>
2026-04-23 01:32:12 +0900

    Update

--- libs/rust/ftl_tcpip/src/ip/ipv4.rs
+++ libs/rust/ftl_tcpip/src/ip/ipv4.rs
@@ -81,18 +81,26 @@ impl fmt::Display for NetMask {
 
 #[repr(C, packed)]
 struct Ipv4Header {
-    version: u8,
-    ihl: u8,
+    version_and_ihl: u8,
     tos: u8,
     len: Ne<u16>,
     id: Ne<u16>,
-    flags: Ne<u16>,
-    frag_offset: Ne<u16>,
+    flags_and_frag_offset: Ne<u16>,
     ttl: u8,
     protocol: u8,
     checksum: Ne<u16>,
     src_addr: Ne<u32>,
     dst_addr: Ne<u32>,
+}
+
+impl Ipv4Header {
+    fn version(&self) -> u8 {
+        self.version_and_ihl >> 4
+    }
+
+    fn ihl(&self) -> u8 {
+        self.version_and_ihl & 0x0f
+    }
 }
 
 #[derive(Debug)]
@@ -104,17 +112,16 @@ pub enum Error {
 
 pub(crate) fn handle_rx(pkt: &mut Packet) -> Result<(), Error> {
     let header = pkt.read::<Ipv4Header>().map_err(Error::PacketRead)?;
+    if header.version() != 4 {
+        return Err(Error::BadVersion(header.version()));
+    }
+
+    if header.ihl() != 5 {
+        return Err(Error::BadHeaderLength(header.ihl()));
+    }
+
     let src = Ipv4Addr::from(header.src_addr);
     let dst = Ipv4Addr::from(header.dst_addr);
-
-    if header.version != 4 {
-        return Err(Error::BadVersion(header.version));
-    }
-
-    if header.ihl != 5 {
-        return Err(Error::BadHeaderLength(header.ihl));
-    }
-
     info!("IPv4 packet: src: {}, dst: {}", src, dst);
     Ok(())
 }
