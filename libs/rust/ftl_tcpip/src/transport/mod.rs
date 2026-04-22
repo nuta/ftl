28c5de54399ef3981cd0af25c0d401e793d148a0
Seiya Nuta <nuta@seiya.me>
2026-04-23 01:41:48 +0900

    Update

--- libs/rust/ftl_tcpip/src/transport/mod.rs
+++ libs/rust/ftl_tcpip/src/transport/mod.rs
@@ -1,3 +1,7 @@
+use core::fmt;
+
+use crate::endian::Ne;
+
 pub mod tcp;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
@@ -5,5 +9,29 @@ pub enum Protocol {
     Tcp,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 pub struct Port(u16);
+
+impl fmt::Debug for Port {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl fmt::Display for Port {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+impl From<Ne<u16>> for Port {
+    fn from(value: Ne<u16>) -> Self {
+        Self(value.into())
+    }
+}
+
+impl From<Port> for Ne<u16> {
+    fn from(value: Port) -> Self {
+        value.0.into()
+    }
+}
--- libs/rust/ftl_tcpip/src/transport/tcp.rs
+++ libs/rust/ftl_tcpip/src/transport/tcp.rs
@@ -2,10 +2,14 @@ use alloc::collections::VecDeque;
 
 use crate::Io;
 use crate::OutOfMemoryError;
+use crate::endian::Ne;
+use crate::ip::ipv4::Ipv4Addr;
+use crate::packet;
 use crate::packet::Packet;
 use crate::route::RouteTable;
 use crate::socket::AnySocket;
 use crate::socket::SocketMap;
+use crate::transport::Port;
 use crate::utils::TryPushBack;
 
 #[derive(Debug)]
@@ -60,13 +64,34 @@ impl<I: Io> TcpListener<I> {
 
 impl<I: Io> AnySocket for TcpListener<I> {}
 
+#[repr(C, packed)]
+struct TcpHeader {
+    src_port: Ne<u16>,
+    dst_port: Ne<u16>,
+    seq: Ne<u32>,
+    ack: Ne<u32>,
+    header_len: u8,
+    flags: u8,
+    window_size: Ne<u16>,
+    checksum: Ne<u16>,
+    urgent_pointer: Ne<u16>,
+}
+
 #[derive(Debug)]
-pub(crate) enum RxError {}
+pub(crate) enum RxError {
+    PacketRead(packet::ReserveError),
+}
 
 pub(crate) fn handle_rx<I: Io>(
     routes: &mut RouteTable<I::Device>,
     sockets: &mut SocketMap,
     pkt: &mut Packet,
 ) -> Result<(), RxError> {
-    todo!()
+    let header = pkt.read::<TcpHeader>().map_err(RxError::PacketRead)?;
+    let src_port = Port::from(header.src_port);
+    let dst_port = Port::from(header.dst_port);
+
+    trace!("TCP packet: src_port: {}, dst_port: {}", src_port, dst_port);
+
+    Ok(())
 }
