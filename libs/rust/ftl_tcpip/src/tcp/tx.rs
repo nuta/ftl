63558c9e0c7123dba87e1346f837855f8fe14694
Seiya Nuta <nuta@seiya.me>
2026-04-26 16:55:33 +0900

    Update

--- libs/rust/ftl_tcpip/src/tcp/tx.rs
+++ libs/rust/ftl_tcpip/src/tcp/tx.rs
@@ -19,6 +19,12 @@ enum TxError {
     NoDevice,
 }
 
+fn encode_header_len(len: usize) -> u8 {
+    debug_assert_eq!(len % 4, 0);
+    debug_assert!(len / 4 <= 0x0f);
+    ((len / 4) as u8) << 4
+}
+
 fn transmit_segment<I: Io>(
     devices: &mut DeviceMap<I::Device>,
     routes: &mut RouteTable,
@@ -26,18 +32,24 @@ fn transmit_segment<I: Io>(
     remote_ip: IpAddr,
     payload: &[u8],
 ) -> Result<(), TxError> {
+    // We'll fill the header length and checksum later.
+    debug_assert_eq!(header.header_len, 0);
+    debug_assert_eq!(Into::<u16>::into(header.checksum), 0);
+
     let head_room = size_of::<EthernetHeader>() + size_of::<Ipv4Header>() + size_of::<TcpHeader>();
     let mut pkt = Packet::new(payload.len(), head_room).map_err(TxError::PacketAlloc)?;
     pkt.write_back_bytes(payload)
         .map_err(TxError::PacketWrite)?;
 
-        let Some(route) = routes.lookup_by_dest(remote_ip) else {
-            return Err(TxError::NoRoute);
-        };
+    let Some(route) = routes.lookup_by_dest(remote_ip) else {
+        return Err(TxError::NoRoute);
+    };
 
-        let Some(device) = devices.get_mut(route.device_id()) else {
-            return Err(TxError::NoDevice);
-        };
+    let Some(device) = devices.get_mut(route.device_id()) else {
+        return Err(TxError::NoDevice);
+    };
+
+    header.header_len = encode_header_len(size_of::<TcpHeader>());
 
     match remote_ip {
         IpAddr::V4(remote_ipv4) => {
