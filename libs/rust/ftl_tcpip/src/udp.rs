611b3c78d14977bc94afdb562b35404336292f74
Seiya Nuta <nuta@seiya.me>
2026-05-03 17:18:58 +0900

    Update

--- libs/rust/ftl_tcpip/src/udp.rs
+++ libs/rust/ftl_tcpip/src/udp.rs
@@ -2,16 +2,19 @@ use alloc::sync::Arc;
 use core::marker::PhantomData;
 
 use crate::TcpIp;
+use crate::checksum::Checksum;
 use crate::endian::Ne;
 use crate::ethernet::EthernetHeader;
 use crate::io::Io;
 use crate::ip::IpAddr;
+use crate::ip::Ipv4Addr;
 use crate::ip::ipv4::Ipv4Header;
 use crate::packet::Packet;
 use crate::packet::WriteableToPacket;
 use crate::packet::{self};
 use crate::socket::Endpoint;
 use crate::transport::Port;
+use crate::transport::Protocol;
 
 pub struct UdpHandle<I: Io>(pub(crate) Arc<UdpSocket<I>>);
 
@@ -28,26 +31,66 @@ impl<I: Io> UdpSocket<I> {
         }
     }
 
-    pub fn send(&self, pkt: &mut Packet, remote: Endpoint, data: &[u8]) -> Result<(), TxError> {
-        let len: u16 = data
-            .len()
+    pub fn send(
+        &self,
+        tcpip: &mut TcpIp<I>,
+        pkt: &mut Packet,
+        remote: Endpoint,
+        data: &[u8],
+    ) -> Result<(), TxError> {
+        let udp_len: u16 = (data.len() + size_of::<UdpHeader>())
             .try_into()
             .map_err(|_| TxError::DataTooLong(data.len()))?;
-        let header = UdpHeader {
+
+        let mut header = UdpHeader {
             src_port: self.local_port.into(),
             dst_port: remote.port.into(),
-            len: len.into(),
+            len: udp_len.into(),
             checksum: 0.into(),
         };
 
-        let head_room =
-            size_of::<EthernetHeader>() + size_of::<Ipv4Header>() + size_of::<UdpHeader>();
-        let mut pkt = Packet::new(data.len(), head_room).map_err(TxError::PacketAlloc)?;
+        let Some((iface, next_hop)) = tcpip.lookup_route(remote.addr) else {
+            return Err(TxError::NoRoute);
+        };
 
-        pkt.write_front(header).map_err(TxError::PacketWrite)?;
-        pkt.write_back_bytes(data).map_err(TxError::PacketWrite)?;
+        match (remote.addr, next_hop) {
+            (IpAddr::V4(remote_ipv4), IpAddr::V4(next_hop)) => {
+                let Some(local_ipv4) = iface.ipv4_addr() else {
+                    return Err(TxError::NoLocalIpv4);
+                };
+
+                let head_room =
+                    size_of::<EthernetHeader>() + size_of::<Ipv4Header>() + size_of::<UdpHeader>();
+                let mut pkt = Packet::new(data.len(), head_room).map_err(TxError::PacketAlloc)?;
+                header.checksum =
+                    compute_udp_checksum(local_ipv4, remote_ipv4, udp_len, pkt.slice()).into();
+                pkt.write_front(header).map_err(TxError::PacketWrite)?;
+                pkt.write_back_bytes(data).map_err(TxError::PacketWrite)?;
+
+                crate::ip::ipv4::transmit::<I>(
+                    iface,
+                    &mut pkt,
+                    remote_ipv4,
+                    next_hop,
+                    local_ipv4,
+                    Protocol::Udp,
+                )
+                .map_err(TxError::Ipv4Tx)?;
+            }
+        }
+
         Ok(())
     }
+}
+
+fn compute_udp_checksum(src_ip: Ipv4Addr, dst_ip: Ipv4Addr, udp_len: u16, payload: &[u8]) -> u16 {
+    let mut checksum = Checksum::new();
+    checksum.supply_u32(src_ip.as_u32());
+    checksum.supply_u32(dst_ip.as_u32());
+    checksum.supply_u16(Protocol::Udp as u16);
+    checksum.supply_u16(udp_len as u16);
+    checksum.supply_bytes(payload);
+    checksum.finish()
 }
 
 #[derive(Debug)]
@@ -55,6 +98,9 @@ pub enum TxError {
     DataTooLong(usize),
     PacketAlloc(packet::AllocError),
     PacketWrite(packet::ReserveError),
+    NoRoute,
+    NoLocalIpv4,
+    Ipv4Tx(crate::ip::ipv4::TxError),
 }
 
 #[derive(Debug)]
