6286ece0321bb318acdbf74ea6b1e27899b3df00
Seiya Nuta <nuta@seiya.me>
2026-09-03 21:57:19 +0900

    Replace bump allocator with ftl_malloc

--- libs/ftl/src/allocator.rs
+++ libs/ftl/src/allocator.rs
@@ -3,7 +3,7 @@ use core::alloc::Layout;
 use core::cell::UnsafeCell;
 use core::ptr;
 
-use ftl_bump_allocator::BumpAllocator;
+use ftl_malloc::LinkedListAllocator;
 use ftl_utils::spinlock::SpinLock;
 
 const HEAP_SIZE: usize = 8 * 1024 * 1024;
@@ -18,7 +18,7 @@ static GLOBAL_ALLOCATOR: GlobalAllocator = GlobalAllocator::new();
 static HEAP: Heap = Heap(UnsafeCell::new([0; HEAP_SIZE]));
 
 struct GlobalAllocator {
-    inner: SpinLock<Option<BumpAllocator>>,
+    inner: SpinLock<Option<LinkedListAllocator>>,
 }
 
 impl GlobalAllocator {
@@ -32,15 +32,29 @@ impl GlobalAllocator {
 unsafe impl GlobalAlloc for GlobalAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         let mut inner = self.inner.lock();
+
+        // Initialize the allocator if this is the first allocation.
         let allocator = inner.get_or_insert_with(|| {
-            let start = HEAP.0.get().cast::<u8>() as usize;
-            BumpAllocator::new(start, start + HEAP_SIZE)
+            let mut allocator = LinkedListAllocator::new();
+            let start = HEAP.0.get().cast::<u8>();
+            // SAFETY: `HEAP` is statically allocated, and is exclusive to
+            //         this allocator.
+            unsafe {
+                allocator.add_chunk(start, HEAP_SIZE);
+            }
+            allocator
         });
 
         allocator
-            .alloc(layout.size(), layout.align())
-            .map_or(ptr::null_mut(), |addr| addr as *mut u8)
+            .malloc(layout.size(), layout.align())
+            .unwrap_or(ptr::null_mut())
     }
 
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        let mut inner = self.inner.lock();
+        let allocator = inner.as_mut().unwrap();
+        unsafe {
+            allocator.free(ptr);
+        }
+    }
 }
