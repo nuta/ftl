1089f5824fb391ea4b3d6ef4173c0501421b412c
Seiya Nuta <nuta@seiya.me>
2026-09-09 20:08:54 +0900

    Decrease kernel malloc chunk size

--- kernel/src/memory.rs
+++ kernel/src/memory.rs
@@ -18,7 +18,7 @@ use crate::boot::BootInfo;
 use crate::boot::FreeRam;
 use crate::boot::NUM_MODULES_MAX;
 
-const MALLOC_CHUNK_SIZE: usize = 512 * 1024; // 512 KB
+const MALLOC_CHUNK_SIZE: usize = 128 * 1024; // 128 KB
 
 #[global_allocator]
 static GLOBAL_ALLOCATOR: GlobalAllocator = GlobalAllocator::new();
