9089e16895ff687498847c21df0049c1b9183585
Seiya Nuta <nuta@seiya.me>
2026-09-20 01:14:13 +0900

    Implement `CLOCK_REALTIME` using monotonic time

--- lx/src/syscall/clock_gettime.rs
+++ lx/src/syscall/clock_gettime.rs
@@ -5,6 +5,7 @@ use crate::types::c_int;
 use crate::types::c_long;
 use crate::types::errno::Errno;
 use crate::types::sys::time::CLOCK_MONOTONIC;
+use crate::types::sys::time::CLOCK_REALTIME;
 use crate::types::sys::time::time_t;
 use crate::types::sys::time::timespec;
 
@@ -13,21 +14,35 @@ pub fn sys_clock_gettime(
     clockid: c_int,
     tp: *mut timespec,
 ) -> Result<c_long, Errno> {
-    if clockid != CLOCK_MONOTONIC {
-        warn!("clock_gettime: unsupported clockid: {}", clockid);
-        return Err(Errno::EINVAL);
-    }
-
     if tp.is_null() {
         return Err(Errno::EFAULT);
     }
 
-    let nanos = ftl::time::now().as_nanos();
-    unsafe {
-        tp.write(timespec {
-            tv_sec: (nanos / 1_000_000_000) as time_t,
-            tv_nsec: (nanos % 1_000_000_000) as c_long,
-        });
+    match clockid {
+        CLOCK_MONOTONIC => {
+            let nanos = ftl::time::now().as_nanos();
+            unsafe {
+                tp.write(timespec {
+                    tv_sec: (nanos / 1_000_000_000) as time_t,
+                    tv_nsec: (nanos % 1_000_000_000) as c_long,
+                });
+            }
+            Ok(0)
+        }
+        CLOCK_REALTIME => {
+            // TODO: Implement CLOCK_REALTIME properly. Currently the monotonic clock is used.
+            let nanos = ftl::time::now().as_nanos();
+            unsafe {
+                tp.write(timespec {
+                    tv_sec: (nanos / 1_000_000_000) as time_t,
+                    tv_nsec: (nanos % 1_000_000_000) as c_long,
+                });
+            }
+            Ok(0)
+        }
+        _ => {
+            warn!("clock_gettime: unsupported clockid: {}", clockid);
+            Err(Errno::EINVAL)
+        }
     }
-    Ok(0)
 }
--- lx/src/types/sys/time.rs
+++ lx/src/types/sys/time.rs
@@ -1,6 +1,7 @@
 use crate::types::c_int;
 use crate::types::c_long;
 
+pub const CLOCK_REALTIME: c_int = 0;
 pub const CLOCK_MONOTONIC: c_int = 1;
 
 pub type time_t = c_long;
