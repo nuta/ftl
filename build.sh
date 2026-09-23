7b4d194c604bad20199742182d08256ff33456a8
Seiya Nuta <nuta@seiya.me>
2026-09-23 14:04:43 +0900

    Remove redundant llvm-strip

--- build.sh
+++ build.sh
@@ -10,7 +10,6 @@ build_rust_app() {
   cargo build --release --target x86_64-unknown-linux-musl
   popd
   cp "apps/${name}/target/x86_64-unknown-linux-musl/release/${name}" "initfs/bin/${name}"
-  llvm-strip "initfs/bin/${name}"
 }
 
 build_default_initfs() {
